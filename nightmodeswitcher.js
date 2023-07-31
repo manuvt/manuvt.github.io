4b6384dec838d1594dbae7571b1393d2eead6693
m@vt <manuvt@users.noreply.github.com>
2023-08-01 05:07:31 +0530

    Update nightmodeswitcher.js

--- nightmodeswitcher.js
+++ nightmodeswitcher.js
@@ -1,31 +1,10 @@
 <script>
-    // Get the dark mode slider input element
-    const darkModeSlider = document.getElementById('darkModeSlider');
-
-    // Function to enable dark mode
-    function enableDarkMode() {
-        document.body.classList.add('dark-mode');
+    document.getElementById('btnSwitch').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
     }
-
-    // Function to disable dark mode
-    function disableDarkMode() {
-        document.body.classList.remove('dark-mode');
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
     }
-
-    // Function to toggle dark mode based on the slider value
-    function toggleDarkMode() {
-        if (darkModeSlider.value === '1') {
-            enableDarkMode();
-        } else {
-            disableDarkMode();
-        }
-    }
-
-    // Event listener to handle dark mode slider changes
-    darkModeSlider.addEventListener('input', toggleDarkMode);
-
-    // Check if dark mode is enabled or not on page load (optional)
-    // You can save the user's preference in localStorage and load it here
-    // For simplicity, we are assuming dark mode is initially disabled
-    disableDarkMode();
+})
 </script>
